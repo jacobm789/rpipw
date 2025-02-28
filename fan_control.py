345d79a8267b936386d967e04372f3d1b792b5b0
Jacob Mayes <jacobamayes@gmail.com>
2025-02-27 21:06:26 -0800

    Reorganized, cleaned up.

--- fan_control.py
+++ fan_control.py
@@ -13,6 +13,20 @@ password = credentials.PASSWORD
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 
+fans = machine.Pin(16, machine.Pin.OUT)
+
+def get_time():
+    ntptime.settime()
+    rtc = machine.RTC()
+    utc_time = rtc.datetime()
+    timezone_offset_hours = -8
+    timezone_offset_seconds = timezone_offset_hours * 3600
+    year, month, day, weekday, hour, minute, second, _ = utc_time
+    seconds = time.mktime((year, month, day, hour, minute, second, weekday, 0))
+    local_seconds = seconds + timezone_offset_seconds
+    local_time = time.localtime(local_seconds)
+    rtc.datetime((local_time[0], local_time[1], local_time[2], local_time[6], local_time[3], local_time[4], local_time[5], 0))
+
 def connect_wifi():
     if not wlan.isconnected():
         wlan.connect(ssid, password)
@@ -30,53 +44,53 @@ def connect_wifi():
         return True
     return True
 
-def get_time():
-    ntptime.settime()
-    rtc = machine.RTC()
-    utc_time = rtc.datetime()
-    timezone_offset_hours = -8
-    timezone_offset_seconds = timezone_offset_hours * 3600
-    year, month, day, weekday, hour, minute, second, _ = utc_time
-    seconds = time.mktime((year, month, day, hour, minute, second, weekday, 0))
-    local_seconds = seconds + timezone_offset_seconds
-    local_time = time.localtime(local_seconds)
-    rtc.datetime((local_time[0], local_time[1], local_time[2], local_time[6], local_time[3], local_time[4], local_time[5], 0))
+def status(session):
+    fans_status = "ON" if fans.value() else "OFF"
+    session.sendall(f"Fans are currently {fans_status}\n".encode())
+    session.sendall(f"The time is {time.localtime()[3]}:{time.localtime()[4]}:{time.localtime()[5]}\n")
 
-connect_wifi()
+def process_command(session, cmd):
+    """Executes the given command."""
+    if cmd == "?":
+        session.sendall(f'Available commands are "fans on", "fans off", "status", "reboot", and "?"\n')
+        session.sendall(f'Use ctrl+c to exit.\n')
+    elif cmd == "fans on":
+        fans.value(1)
+        session.sendall(b"Fans are now ON\n")
+    elif cmd == "fans off":
+        fans.value(0)
+        session.sendall(b"Fans are now OFF\n")
+    elif cmd == "status":
+        status(session)
+    elif cmd == "reboot":
+        session.sendall(b"Rebooting...\n")
+        machine.reset()
+    else:
+        session.sendall(b"Unknown command\n")
 
-led = machine.Pin("LED", machine.Pin.OUT)
-fans = machine.Pin(16, machine.Pin.OUT)
-
-
-commands = ["led on", "led off", "fans on", "fans off", "status", "reboot"]
-
-def handle_input(conn):
+def handle_input(session):
     cmd_buffer = ""
 
-    conn.sendall(b"Welcome to Pico W Server!\n")
-    status = "ON" if led.value() else "OFF"
-    status2 = "ON" if fans.value() else "OFF"
-    conn.sendall(f"LED is currently {status}\n".encode())
-    conn.sendall(f"Fans are currently {status2}\n".encode())
-    conn.sendall(f'Available commands are "{'", "'.join(str(i) for i in commands)}", and "?"\n')
-    conn.sendall(f"The time is {time.localtime()[3]}:{time.localtime()[4]}:{time.localtime()[5]}\n")
-    conn.sendall(f'Use ctrl+c to exit.\n>>> ')
+    session.sendall(b"Welcome to Pico W Server!\n")
+    session.sendall(f'Available commands are "fans on", "fans off", "status", "reboot", and "?"\n')
+    status(session)
+    session.sendall(f'Use ctrl+c to exit.\n>>> ')
 
     start_time = time.time()
 
     poller = uselect.poll()
-    poller.register(conn, uselect.POLLIN)
+    poller.register(session, uselect.POLLIN)
 
     while True:
         if time.time() - start_time > 60*3:
-            conn.sendall(b"\nConnection timed out. Closing.\n")
+            session.sendall(b"\nConnection timed out. Closing.\n")
             break
 
         events = poller.poll(1000)
         if not events:
             continue
 
-        data = conn.recv(1)
+        data = session.recv(1)
 
         if not data:
             break
@@ -85,43 +99,14 @@ def handle_input(conn):
 
         if char == "\n":
             if cmd_buffer.strip():
-                process_command(conn, cmd_buffer.strip())
+                process_command(session, cmd_buffer.strip())
             cmd_buffer = ""
-            conn.sendall(b">>> ")
+            session.sendall(b">>> ")
 
         else:
             cmd_buffer += char
 
-    conn.close()
-
-def process_command(conn, cmd):
-    """Executes the given command."""
-    if cmd == "?":
-        conn.sendall(f'Available commands are "{'", "'.join(str(i) for i in commands)}", and "?"\n')
-        conn.sendall(f'Use ctrl+c to exit.\n')
-    elif cmd == "led on":
-        led.value(1)
-        conn.sendall(b"LED is now ON\n")
-    elif cmd == "led off":
-        led.value(0)
-        conn.sendall(b"LED is now OFF\n")
-    elif cmd == "fans on":
-        fans.value(1)
-        conn.sendall(b"Fans are now ON\n")
-    elif cmd == "fans off":
-        fans.value(0)
-        conn.sendall(b"Fans are now OFF\n")
-    elif cmd == "status":
-        status = "ON" if led.value() else "OFF"
-        status2 = "ON" if fans.value() else "OFF"
-        conn.sendall(f"LED is currently {status}\n".encode())
-        conn.sendall(f"Fans are currently {status2}\n".encode())
-        conn.sendall(f"The time is {time.localtime()[3]}:{time.localtime()[4]}:{time.localtime()[5]}\n")
-    elif cmd == "reboot":
-        conn.sendall(b"Rebooting...\n")
-        machine.reset()
-    else:
-        conn.sendall(b"Unknown command\n")
+    session.close()
 
 def shell_server():
     addr = ("0.0.0.0", 23)
@@ -143,9 +128,11 @@ def shell_server():
             elif time.localtime()[3:5] == (16, 00):
                 fans.value(0)
             s.settimeout(10)
-            conn, addr = s.accept()
-            handle_input(conn)
+            session, addr = s.accept()
+            handle_input(session)
         except OSError:
             pass
+
+connect_wifi()
 
 shell_server()
